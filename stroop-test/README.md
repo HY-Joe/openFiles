ec1d429d13e19175abdfc3f172f869a7952cd747
HY-Joe <62640950+HY-Joe@users.noreply.github.com>
2020-10-02 11:13:30 +0900

    test

---
+++ stroop-test/README.md
@@ -0,0 +1,6 @@
+# stroop-test
+
+Stroop test game written in HTML, CSS, and JavaScript.
+
+Play this game at [strooptest.mrcodeswildride.com](https://strooptest.mrcodeswildride.com/).
+Learn how to make this game at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
---
+++ stroop-test/script.js
@@ -0,0 +1,63 @@
+let wordParagraph = document.getElementById(`wordParagraph`)
+let startButton = document.getElementById(`startButton`)
+let colorButtons = document.getElementsByClassName(`color`)
+
+let count = 0
+let startTime
+
+startButton.addEventListener(`click`, start)
+
+for (let button of colorButtons) {
+  button.addEventListener(`click`, selectColor)
+}
+
+function start() {
+  startButton.style.display = `none`
+
+  for (let button of colorButtons) {
+    button.style.display = `inline`
+  }
+
+  count = 0
+  startTime = Date.now()
+  pickRandomColor()
+}
+
+function pickRandomColor() {
+  let randomNumber = Math.floor(Math.random() * colorButtons.length)
+  wordParagraph.innerHTML = colorButtons[randomNumber].innerHTML
+
+  randomNumber = Math.floor(Math.random() * colorButtons.length)
+  wordParagraph.style.color = colorButtons[randomNumber].innerHTML
+
+  count = count + 1
+}
+
+function selectColor() {
+  if (this.innerHTML == wordParagraph.style.color) {
+    //if (count < 5) {
+    if (count < 10) {
+      pickRandomColor()
+    }
+    else {
+      let time = (Date.now() - startTime) / 1000
+      wordParagraph.innerHTML = `Time: ${time} seconds`
+
+      wordParagraph.style.color = `black`
+      startButton.style.display = `inline`
+
+      for (let button of colorButtons) {
+        button.style.display = `none`
+      }
+    }
+  }
+  else {
+//    wordParagraph.innerHTML = `You lose`
+//    wordParagraph.style.color = `black`
+//    startButton.style.display = `inline`
+//
+//    for (let button of colorButtons) {
+//      button.style.display = `none`
+//    }
+  }
+}
